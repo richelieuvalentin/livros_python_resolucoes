c0e5777f13e3f8814e9fb75911dd01265068da45
Richelieu Valentin <richelieuvalentin@gmail.com>
2026-03-14 17:19:07 -0300

    Capitulo 2 Use a Cabeça

---
+++ use_a_cabeca_aprenda_programar/ch2/dgo_age.py
@@ -0,0 +1,11 @@
+# Pergunta ao usuário o nome e idade do cachorroe guarda em variavéis
+dog_name = input("What is your dog's name? ")
+dog_age = int(input("What is your dog's age? "))
+
+# Converte para anos humanos
+human_age = dog_age * 7
+
+# Output para o usuário
+print(f"Your dog's age is {human_age}, years old in human years")
+
+
